fc4aa96dcd3b4acda68220234ff71ad97a54a12f
Kim JinSeon <kwlstjs00@gmail.com>
2024-05-22 16:42:22 +0900

    Quartz sync: May 22, 2024, 4:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "blog.ziinsun.com",
+    pageTitle: "🎁 ziinsun",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
