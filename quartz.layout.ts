d72eda922ab5979b7cdff5d1303daab70cae4948
Kim JinSeon <kwlstjs00@gmail.com>
2024-05-27 16:18:32 +0900

    Quartz sync: May 27, 2024, 4:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,7 +13,7 @@ export const sharedPageComponents: SharedLayout = {
 }
 
 // components for pages that display a single page (e.g. a single note)
-export const defaultContentPageLayout: PageLayout = {
+export const defaultContentPageLayout: PageLayout = { 
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
